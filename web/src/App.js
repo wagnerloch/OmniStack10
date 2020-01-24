211506476c6379f46dc9429924a73fb577953f0b
Wagner <wagner_loch@hotmail.com>
2020-01-24 19:58:45 -0300

    web complete

--- web/src/App.js
+++ web/src/App.js
@@ -1,140 +1,50 @@
 import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+
 function App() {
 
-    const [github_username, setGithubUsername] = useState('');
-    const [techs, setTechs] = useState('');
-    const [latitude, setLatitude] = useState('');
-    const [longitude, setLongitude] = useState('');
+    const [devs, setDevs] = useState([]);
+
+
+    
 
     useEffect(() => {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                const { latitude, longitude } = position.coords;
+        async function loadDevs () {
+            const response = await api.get('/devs');
 
-                setLatitude(latitude);
-                setLongitude(longitude);
-            },
-            (err) => {
-                console.log(err);
-            },
-            {
-                timeout: 30000,
-            }
-        )
+            setDevs(response.data);
+        }
+
+        loadDevs();
     }, []);
 
-    async function handleAddDev (e) {
-        e.preventDefault();
+    async function handleAddDev (data) {
+        const response = await api.post('/devs', data)
+
+        
+
+        setDevs([...devs, response.data]);
     }
 
     return ( 
         <div id="app">
             <aside>
                 <strong>Cadastrar</strong>
-                <form onSubmit={handleAddDev}>
-                    <div className="input-block">
-                        <label htmlFor="github_username">Usuário do GitHub</label>
-                        <input 
-                            name="github_username" 
-                            id="github_username" 
-                            required
-                            value={github_username}
-                            onChange={e => setGithubUsername(e.target.value)}
-                        />
-                    </div>                    
-                    
-                    <div className="input-block">
-                        <label htmlFor="techs">Tecnologias</label>
-                        <input 
-                            name="techs" 
-                            id="techs" 
-                            required
-                            value={techs}
-                            onChange={e => setTechs(e.target.value)}
-                        />
-                    </div>
-
-                    <div className="input-group">
-                        <div className="input-block">
-                            <label htmlFor="latitude">Latitude</label>
-                            <input 
-                                type="number" 
-                                name="latitude" 
-                                id="latitude" 
-                                required 
-                                value={latitude}
-                                onChange={e => setLatitude(e.target.value)}
-                            />
-                        </div>
-
-                        <div className="input-block">
-                            <label htmlFor="longitude">Longitude</label>
-                            <input 
-                                type="number" 
-                                name="longitude" 
-                                id="longitude" 
-                                required 
-                                value={longitude}
-                                onChange={e => setLongitude(e.target.value)}
-                            />
-                        </div>
-                    </div>
-
-                    <button type="submit">Salvar</button>               
-                </form>
+                <DevForm onSubmit={handleAddDev} />
             </aside>
             <main>
                 <ul>
-                <li className="dev-item">
-                        <header>
-                            <img src="https://avatars3.githubusercontent.com/u/10766716?s=460&v=4" alt="Wagner Loch"/>
-                            <div className="user-info">
-                                <strong>Wagner Loch</strong>
-                                <span>ReactJS, React Native, Node.js</span>
-                            </div>
-                        </header>
-                        <p>Parágrafo vindo do github sobre o usuário.</p>
-                        <a href="https://github.com/wagnerloch">Acessar perfil no GitHub</a>
-                    </li>
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars3.githubusercontent.com/u/10766716?s=460&v=4" alt="Wagner Loch"/>
-                            <div className="user-info">
-                                <strong>Wagner Loch</strong>
-                                <span>ReactJS, React Native, Node.js</span>
-                            </div>
-                        </header>
-                        <p>Parágrafo vindo do github sobre o usuário.</p>
-                        <a href="https://github.com/wagnerloch">Acessar perfil no GitHub</a>
-                    </li>
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars3.githubusercontent.com/u/10766716?s=460&v=4" alt="Wagner Loch"/>
-                            <div className="user-info">
-                                <strong>Wagner Loch</strong>
-                                <span>ReactJS, React Native, Node.js</span>
-                            </div>
-                        </header>
-                        <p>Parágrafo vindo do github sobre o usuário.</p>
-                        <a href="https://github.com/wagnerloch">Acessar perfil no GitHub</a>
-                    </li>
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars3.githubusercontent.com/u/10766716?s=460&v=4" alt="Wagner Loch"/>
-                            <div className="user-info">
-                                <strong>Wagner Loch</strong>
-                                <span>ReactJS, React Native, Node.js</span>
-                            </div>
-                        </header>
-                        <p>Parágrafo vindo do github sobre o usuário.</p>
-                        <a href="https://github.com/wagnerloch">Acessar perfil no GitHub</a>
-                    </li>
+                    {devs.map(dev => (
+                       <DevItem key={dev._id} dev={dev} /> 
+                    ))}
                 </ul>
             </main>
         </div>
